2443a4753fe908221f116d139d2f8af1b8b39106
Johan Gustafsson <johan.gustafsson@solidio.se>
2018-01-21 13:00:23 +0100

    Removes spaces from function declarations

--- plugin/index.py
+++ plugin/index.py
@@ -2,19 +2,19 @@ import re
 import sys
 import vim
 
-def getSearchOutputString( formattedQuery ):
+def getSearchOutputString(formattedQuery):
   return ("Ack " +  "\"" + formattedQuery + "\"" + " -i")
 
-def espaceCharactersInString( string ):
+def espaceCharactersInString(string):
   return re.escape(string)
 
-def getIgnoreDirString( ignoreDir ):
+def getIgnoreDirString(ignoreDir):
   if len(ignoreDir) is 0:
     return ""
   else:
     return " --ignore-dir=" + " --ignore-dir=".join(map(str, ignoreDir))
 
-def searchWithAck( selectedText, ignoreDirs ):
+def searchWithAck(selectedText, ignoreDirs):
   escapedString = espaceCharactersInString(selectedText)
   vim.command(getSearchOutputString(escapedString) + getIgnoreDirString(ignoreDirs))
 
