025d3862b58b438d2a8b25268d483c2d42498253
Sebastian karlsson <sebbekarlsson97@gmail.com>
2018-01-22 08:35:26 +0100

    pep8 requires indentation to be 4 spaces

--- plugin/index.py
+++ plugin/index.py
@@ -4,33 +4,33 @@ import vim
 
 
 def getSearchOutputString(formattedQuery):
-  return ("Ack " +  "\"" + formattedQuery + "\"" + " -i")
+    return ("Ack " +  "\"" + formattedQuery + "\"" + " -i")
 
 
 def espaceCharactersInString(string):
-  return re.escape(string)
+    return re.escape(string)
 
 
 def getIgnoreDirString(ignoreDir):
-  if len(ignoreDir) is 0:
-    return ""
+    if len(ignoreDir) is 0:
+        return ""
   else:
-    return " --ignore-dir=" + " --ignore-dir=".join(map(str, ignoreDir))
+      return " --ignore-dir=" + " --ignore-dir=".join(map(str, ignoreDir))
 
 
 def searchWithAck(selectedText, ignoreDirs):
-  escapedString = espaceCharactersInString(selectedText)
+    escapedString = espaceCharactersInString(selectedText)
   vim.command(getSearchOutputString(escapedString) + getIgnoreDirString(ignoreDirs))
 
 
 def printErrorMessage():
-  print("format-ack-search-from-selection: No selected text")
+    print("format-ack-search-from-selection: No selected text")
 
 # Get values from VIM script
 selectedText = vim.eval('g:selectedText')
 ignoreDirs = vim.eval('g:format_ack_search_from_selection_ignore_dirs')
 
 if selectedText:
-  searchWithAck(selectedText, ignoreDirs)
+    searchWithAck(selectedText, ignoreDirs)
 else:
-  printErrorMessage()
+    printErrorMessage()
